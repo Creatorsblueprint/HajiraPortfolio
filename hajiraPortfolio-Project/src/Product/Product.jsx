4ee9dbc8236496753a1782a7e81158b84dc7063f
LebohangDev <phxsoul39@gmail.com>
2026-01-20 23:16:09 +0400

    regex change

--- hajiraPortfolio-Project/src/Product/Product.jsx
+++ hajiraPortfolio-Project/src/Product/Product.jsx
@@ -11,7 +11,7 @@ function ProductSection() {
 
   const handleEmail = (email) => {
     setEmail(email);
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     setIsEmailValid(emailRegex.test(email));
   }
 
