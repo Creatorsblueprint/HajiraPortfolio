99e11ebd3fbd1050c83500b0819feef18338ab43
LebohangDev <phxsoul39@gmail.com>
2026-01-20 23:28:01 +0400

    minor change to footer

--- hajiraPortfolio-Project/src/Footer/Footer.jsx
+++ hajiraPortfolio-Project/src/Footer/Footer.jsx
@@ -11,60 +11,37 @@ function Footer({ setActive, active }) {
                     </div>
                     <div className={styles.footerNav}>
                         <div className={active === 'Home' ? styles.activeNav : styles.notActiveNav}>
-                            <h1><Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Home</Link></h1>
+                            <h1><Link to="/">Home</Link></h1>
                         </div>
 
                         <div className={active === "Product" ? styles.activeNav : styles.notActiveNav}>
-                            <h1><Link to="/product" style={{ color: 'inherit', textDecoration: 'none' }}>Product</Link></h1>
+                            <h1><Link to="/product">Product</Link></h1>
                         </div>
 
                         <div className={active === 'Contact' ? styles.activeNav : styles.notActiveNav}>
-                            <h1><a href="mailto:workwithhajira@gmail.com" style={{ color: 'inherit', textDecoration: 'none' }}>Contact</a></h1>
+                            <h1><a href="mailto:workwithhajira@gmail.com">Contact</a></h1>
                         </div>
                     </div>
                     <div className={styles.footerSocials}>
                         <img onClick={(e) => { e.preventDefault(); window.location.href = 'https://www.instagram.com/hajirakhaaan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='; }} src="Images/socialIcons/instagram-Icon.png" alt="instagram" />
                         <img onClick={(e) => { e.preventDefault(); window.location.href = 'https://www.tiktok.com/@hajirakhaaan?is_from_webapp=1&sender_device=pc'; }} src="Images/socialIcons/tiktok-Icon.png" alt="tiktok" />
-                        <img onClick={(e) => { e.preventDefault(); window.location.href = 'mailto:workwithhajira@gmail.com'; }} src="Images/socialIcons/gmail-Icon.png" alt="tiktok" />
+                        <img onClick={(e) => { e.preventDefault(); window.location.href = 'mailto:workwithhajira@gmail.com'; }} src="Images/socialIcons/gmail-Icon.png" alt="gmail" />
                     </div>
-
-
-
 
                     <div className={styles.footerBottom}>
                         <div className={styles.footerLine}>
                             <hr />
-
                         </div>
                         <div className={styles.footerBottomContainer}>
                             <div className={styles.footerCopyright}>
-                                <p>Copyright 2025</p>
-                                <i class="ri-copyright-line"></i>
-
+                                <p>Copyright 2026</p>
+                                <i className="ri-copyright-line"></i>
                             </div>
                             <div className={styles.dev}>
-                                <p>Developed By LDEV</p>
+                                <p>Developed By CreatorsBlueprint</p>
                             </div>
-
-                            <div className={styles.Socials}>
-
-                                <img onClick={(e) => { e.preventDefault(); window.location.href = 'https://www.instagram.com/hajirakhaaan?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='; }} src="Images/socialIcons/instagram-Icon.png" alt="instagram" />
-                                <img onClick={(e) => { e.preventDefault(); window.location.href = 'https://www.tiktok.com/@hajirakhaaan?is_from_webapp=1&sender_device=pc'; }} src="Images/socialIcons/tiktok-Icon.png" alt="tiktok" />
-                                <img onClick={(e) => { e.preventDefault(); window.location.href = 'mailto:workwithhajira@gmail.com'; }} src="Images/socialIcons/gmail-Icon.png" alt="tiktok" />
-
-                            </div>
-
                         </div>
-
-
-
-
-
                     </div>
-
-
-
-
                 </div>
             </div >
 
