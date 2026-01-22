c6e0d0c37135ecbc77dd437932c5e5fa40a03d68
LebohangDev <phxsoul39@gmail.com>
2026-01-22 17:04:16 +0400

    redirect back to product section

--- hajiraPortfolio-Project/src/Product/Product.jsx
+++ hajiraPortfolio-Project/src/Product/Product.jsx
@@ -74,8 +74,8 @@ function ProductSection() {
           amount: roundedPrice, // backend multiplies by 100
           email: email,
           currency: code,
-          successUrl: "https://lebohangdev.github.io/HajiraPortfolio/?payment=success",
-          cancelUrl: "https://lebohangdev.github.io/HajiraPortfolio/?payment=cancel",
+          successUrl: "https://lebohangdev.github.io/HajiraPortfolio/?payment=success/#/product",
+          cancelUrl: "https://lebohangdev.github.io/HajiraPortfolio/?payment=cancel/#/product",
         }),
       });
 
