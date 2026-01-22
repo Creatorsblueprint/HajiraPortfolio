c47c6662936b576d010cdda8a3c8290a3b5b251c
LebohangDev <phxsoul39@gmail.com>
2026-01-22 17:14:27 +0400

    minor change

--- hajiraPortfolio-Project/src/Product/Product.jsx
+++ hajiraPortfolio-Project/src/Product/Product.jsx
@@ -74,8 +74,8 @@ function ProductSection() {
           amount: roundedPrice, // backend multiplies by 100
           email: email,
           currency: code,
-          successUrl: "https://lebohangdev.github.io/HajiraPortfolio/#/product?payment=success",
-          cancelUrl: "https://lebohangdev.github.io/HajiraPortfolio/#/product?payment=cancel",
+          successUrl: "https://lebohangdev.github.io/HajiraPortfolio/#/product/?payment=success",
+          cancelUrl: "https://lebohangdev.github.io/HajiraPortfolio/#/product/?payment=cancel",
         }),
       });
 
